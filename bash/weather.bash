de7cf5a9a701125176f8f4a6c62eda8480e0aa98
Diogo Almiro <diogoalmiro@gmail.com>
2016-06-12 14:53:01 +0100

    Update weather.bash

--- bash/weather.bash
+++ bash/weather.bash
@@ -1,9 +1,24 @@
-#Displays weather of a certain city, needs internet
+# Displays the current weather of a certain city uses wget
+
+# How to use this function:
+# 	Copy the code to your .bashrc 
+#	Change the default values if you want
+#	Open a terminal
+#	run:
+#		weather City Name
+
+# If you run it without arguments it will use a default value
+# In this case Lisbon but you can change the line
+# `city=${city:-Lisbon}` to the city you want 
+# It is also implemented autocompletion to the city name
+# command -W "List Of Cities separated by spaces" weather
+
 function weather {
 	
 	city="$*"
-	city=${city:-Lisbon}
+	city=${city:-Lisbon} #default value Lisbon
 	old=
+	# convert spaces into %20 
 	while [ "$old" != "$city" ]; do
 		old=$city
 		city=${city/ /%20}
